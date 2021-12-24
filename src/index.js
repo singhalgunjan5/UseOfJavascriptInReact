a14c8007bffc140537184235db53530d6ba96e81
gunjan <singhalgunjan5@gmail.com>
2021-12-24 23:35:31 +0530

    first commit react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const s1="season1";
+const s2="season2";
+const s3="season3";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+  <h1>My Five Favourite Netflix Series or Movies</h1>
+  <p>
+   <ol>
+   <h1>
+  <li> Cobra Kai all seasons {1+1+1}</li>
+  <li> {`lock and key ${s1} ${s2}`}</li>
+  <li> Squid Games</li>
+  <li> Crash landing on you</li>
+  <li> Mismatched</li>
+  </h1>
+   </ol>
+
+
+  </p>
+  </>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
